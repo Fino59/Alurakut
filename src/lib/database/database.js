2df5a5006ccce9220188ca174c930335cf5c6e66
Danilo Fontes <slipdan@hotmail.com>
2021-07-14 21:04:20 -0300

    Create database for Communities

---
+++ src/lib/database/database.js
@@ -0,0 +1,22 @@
+export const baseCommunities = [
+    {id: new Date().toISOString(), title: "Eu odeio acordar cedo!", image: "https://alurakut.vercel.app/capa-comunidade-01.jpg"},
+    {id: new Date().toISOString(), title: "Lênin, de três", image: "https://img10.orkut.br.com/community/f0131f9cec84100d3b7e02bd8a9323c2.jpg"},
+    {id: new Date().toISOString(), title: "Deftones", image: "https://i.imgur.com/rBToEMB.jpg"},
+    {id: new Date().toISOString(), title: "New Metal And Others", image: "https://i.imgur.com/2G1EFvi.jpg"},
+    {id: new Date().toISOString(), title: "Tenho medo da Véia (o) Quaker!", image: "https://i.imgur.com/rCbmWFi.jpg"},
+    {id: new Date().toISOString(), title: "Imagina se pega no olho?", image: "https://i.imgur.com/diTSqR6.jpg"},
+];
+
+export const orkutDocs = [
+    { name: 'Recados', slug: '45', icon: 'book' },
+    { name: 'Fotos', slug: '13', icon: 'camera' },
+    { name: 'Videos', slug: '2', icon: 'video-camera' },
+    { name: 'Fãs', slug: '8', icon: 'star' },
+    { name: 'Mensagens', slug: '2', icon: 'email' }
+];
+
+export const trustCoolSexyCounter = [
+    { name: 'Confiável', slug: '3', icon: 'smile' },
+    { name: 'Legal', slug: '2', icon: 'cool' },
+    { name: 'Sexy', slug: '1', icon: 'heart' }
+]
